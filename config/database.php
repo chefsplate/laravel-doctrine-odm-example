8a9bb85ca2dd51d9560777ee6630570c6f279a1a
David Chang <david.chang@chefsplate.com>
2016-10-13 18:25:56 -0400

    Configure application with providers/facades.

--- config/database.php
+++ config/database.php
@@ -85,6 +85,12 @@ return [
             'prefix'   => '',
         ],
 
+        'mongodb' => array(
+            'driver'   => 'mongodb',
+            'dsn'      => 'mongodb://127.0.0.1:27017',
+            'database' => 'todos'
+        ),
+
     ],
 
     /*
